36569d2cc94d18f513b31e2ec81393c809dbc6e0
kunal <kunalchaugule36@gmail.com>
2026-01-30 11:44:38 +0530

    Fix metrics shape error by replacing tf.numpy_function with pure TF ops

--- metrics.py
+++ metrics.py
@@ -2,9 +2,10 @@ import numpy as np
 import tensorflow as tf
 
 smooth = 1e-15
+
 def dice_coef(y_true, y_pred):
-    y_true = tf.keras.layers.Flatten()(y_true)
-    y_pred = tf.keras.layers.Flatten()(y_pred)
+    y_true = tf.reshape(y_true, [-1])
+    y_pred = tf.reshape(y_pred, [-1])
     intersection = tf.reduce_sum(y_true * y_pred)
     return (2. * intersection + smooth) / (tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) + smooth)
 
@@ -12,10 +13,8 @@ def dice_loss(y_true, y_pred):
     return 1.0 - dice_coef(y_true, y_pred)
 
 def iou(y_true, y_pred):
-    def f(y_true, y_pred):
-        intersection = (y_true * y_pred).sum()
-        union = y_true.sum() + y_pred.sum() - intersection
-        x = (intersection + smooth) / (union + smooth)
-        x = x.astype(np.float32)
-        return x
-    return tf.numpy_function(f, [y_true, y_pred], tf.float32)
+    y_true = tf.reshape(y_true, [-1])
+    y_pred = tf.reshape(y_pred, [-1])
+    intersection = tf.reduce_sum(y_true * y_pred)
+    union = tf.reduce_sum(y_true) + tf.reduce_sum(y_pred) - intersection
+    return (intersection + smooth) / (union + smooth)
